c4650d40b860ff1c9a9e71bb938c742fb88139fb
imniynaiy <zhang00710@gmail.com>
2025-02-28 23:52:34 +0000

    update file creating script

--- python/create_files.sh
+++ python/create_files.sh
@@ -1,3 +1,14 @@
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory_name>"
+    exit 1
+fi
+
+directory_name=$1
+
+cd /workspaces/atcoder/code/python
+mkdir "$directory_name"
+cd "$directory_name"
+
 touch A.py
 touch B.py
 touch C.py
