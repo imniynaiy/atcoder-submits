297ce84df70d51243eb015eb2d04738384daffdf
imniynaiy <zhang00710@gmail.com>
2025-04-05 13:18:30 +0000

    updates C but still TLE

--- python/400/C.py
+++ python/400/C.py
@@ -1,26 +1,52 @@
 n = int(input())
 
-xset = set()
+xset = {}  # Use a dictionary instead of a set
 
 a = 1
-b = 1
+b = 0
 
-x = 2 ** a * b ** 2
-# print('adding', a, b , x)
-xset.add(x)
+twoxxa = []
+
+x = 1
+while x < n:
+    x *= 2
+    twoxxa.append(x)
+
+bsquare = []
+x = 0
+while x < n // 2:
+    x += 2 * b + 1
+    b += 1
+    bsquare.append(x)
+
+i = 0
+j = 0
+if i >= len(twoxxa):
+    print(0)
+    exit()
+if j >= len(bsquare):
+    print(0)
+    exit()
+
+x = twoxxa[i] * bsquare[j]
+# print('adding', twoxxa[i], bsquare[j], x)
+xset[x] = True  # Add to dictionary
+
 while x <= n:
     while x <= n:
-        b += 1
-        x = (2 ** a) * (b ** 2)
+        j += 1
+        x = twoxxa[i] * bsquare[j]
         if x <= n:
-            # print('adding', a, b , x)
-            xset.add(x)
+            # print('adding', twoxxa[i], bsquare[j], x)
+            xset[x] = True  # Add to dictionary
     if x > n:
-        b = 1
-        a += 1
-        x = (2 ** a) * (b ** 2)
+        j = 0
+        i += 1
+        if i >= len(twoxxa):
+            break
+        x = twoxxa[i] * bsquare[j]
         if x <= n:
-            # print('adding', a, b , x)
-            xset.add(x)
+            # print('adding', twoxxa[i], bsquare[j], x)
+            xset[x] = True  # Add to dictionary
 
-print(len(xset))
+print(len(xset))  # Output the size of the dictionary
